f3a8852e5429412a9ed42024f1ec92aefd447420
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-03-25 14:49:52 +0100

    Add files via upload

--- creer-article.php
+++ creer-article.php
@@ -1,57 +1,88 @@
-<html>
-
 <?php
 
-if(isset($_SESSION['login']) and $_SESSION['login']=='modérateur' or $_SESSION['login']=='administrateur'){
+require_once("libraries/utilities.php");
 
-?>
+// if(!isLoged()){
+//     if($_SESSION["login"] != "admin" || $_SESSION["login"] != "modo"){
+//         redirect("index.php");
+//     }
+// }
 
-    <head>
-        <title>Créer Article</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-
-    <header>
-
-    </header>
-
-    <body>
-
-            Une page permettant de créer des articles (creer-article.php) :
-        Cette page possède un formulaire permettant aux modérateurs et
-        administrateurs de créer de nouveaux articles. Le formulaire contient donc
-        le texte de l’article, une liste déroulante contenant les catégories existantes
-        en base de données et un bouton submit.
-
-        <?php
-
-
-
-
-
-
-
-
-        ?>
-
-
-    </body>
-
-    <footer>
-
-    </footer>
-
-
+require("templates/header.phtml"); ?>
+<main id="main-creart">
 <?php
-}
-elseif(empty($_SESSION['login'])){
-    header("location:connexion.php");
-}
-else{
-    header("location:index.php");
 
+////////// UTILISATEURS //////////
+
+$connexion = getCo();
+$requete = "SELECT * FROM `utilisateurs` WHERE login = '".$_SESSION['login']."'";
+$query = mysqli_query($connexion,$requete);
+$resultat = mysqli_fetch_all($query);
+
+////////// CATEGORIES //////////
+
+$requete_cat = "SELECT * FROM categories";
+$query_cat = mysqli_query($connexion, $requete_cat);
+$resultat_cat = mysqli_fetch_all($query_cat,MYSQLI_ASSOC);
+// var_dump($resultat[0][0]);
+// var_dump($resultat_cat);
+
+//////////
+
+$nb_cat = 0;
+
+if(!empty($_SESSION['login'] && $_SESSION['id_droits'] == 42 || $_SESSION['id_droits'] == 1337)): ?>
+    <article id="art-of-creart">
+        <div id="block-creart">
+
+        </div>
+        <div id="block-creart2">
+            <h1 id="creart-title">Créer un artilcle</h1>
+            <form action="#" method="post" id="form-creart">
+                <div id="block-form-creart">
+                    <textarea name="textArt" id="textArt" placeholder="Exprimez-vous..."></textarea>
+                    <div id="select-containt">
+                        <label for="selCat">Choisissez une catégorie !</label>
+                        <select name="selCat" id="selCat">
+                        <?php foreach($resultat_cat as $cat): ?>
+                                <option value="<?php echo $nb_cat ?>"><?php echo $cat["nom"] ?></option>
+                        <?php echo $nb_cat++;
+                        endforeach ?>
+                        </select>
+                    </div>
+                    <button type="submit" name="creer" id="button-creart">Créer</button>
+                </div>
+            </form>
+        </div>
+    </article>
+<?php elseif(empty($_SESSION["login"]) && $_SESSION["id_droits"] == 1):
+
+    $erreur = "Vous devez être connecté en tant qu'administrateur ou modérateur pour accéder a cette page";
+
+endif;
+
+
+if(isset($_POST["creer"])){
+    
+    $art_txt = extractSafeFromPost("textArt");
+    $categorie = filter_input(INPUT_POST, "selCat") +1;
+
+    if($art_txt != ""){
+
+    $requete_insert = "INSERT INTO articles(`id`,`article`,`id_utilisateur`,`id_categorie`,`date`)VALUES(null,'".$art_txt."','".$resultat[0][0]."',$categorie,now())";
+    $query_insert = mysqli_query($connexion,$requete_insert); ?>
+    <meta http-equiv="refresh" content="0;URL=index.php">
+<?php    }else {
+
+    $erreur = "Veuillez inclure du texte a votre article";
+    }
+    
 }
+if(isset($erreur)): ?>
+    <div id="error-txt-creart"><?php echo $erreur ?></div>
+<?php endif; ?>
+
+</main>
+<?php
+require("templates/footer.phtml");
 ?>
-
-
-</html>
