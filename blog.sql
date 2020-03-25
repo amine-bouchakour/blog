44260f637477129313542621ede7334cc2796e39
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-03-25 21:22:06 +0100

    Add files via upload

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 04 mars 2020 à 18:05
+-- Généré le :  mer. 25 mars 2020 à 20:21
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.3.5
 
@@ -32,13 +32,20 @@ USE `blog`;
 
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `article` text NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   `id_categorie` int(11) NOT NULL,
   `date` timestamp NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `articles`
+--
+
+INSERT INTO `articles` (`id`, `article`, `id_utilisateur`, `id_categorie`, `date`) VALUES
+(1, 'azjkmldkjffdskj kjfdmlkjsd:', 1, 1, '2020-03-16 23:00:00');
 
 -- --------------------------------------------------------
 
@@ -48,10 +55,19 @@ CREATE TABLE IF NOT EXISTS `articles` (
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(80) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `nom`) VALUES
+(1, 'Guitare'),
+(2, 'Piano'),
+(3, 'Violon');
 
 -- --------------------------------------------------------
 
@@ -67,7 +83,14 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
   `id_utilisateur` int(11) NOT NULL,
   `date` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id`, `commentaire`, `id_article`, `id_utilisateur`, `date`) VALUES
+(1, 'Jouons de la guitare tous ensemble', 1, 1, '2020-03-25 20:04:09');
 
 -- --------------------------------------------------------
 
@@ -77,10 +100,19 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
 
 DROP TABLE IF EXISTS `droits`;
 CREATE TABLE IF NOT EXISTS `droits` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(80) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1338 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `droits`
+--
+
+INSERT INTO `droits` (`id`, `nom`) VALUES
+(1, 'utilisateur'),
+(42, 'modérateur'),
+(1337, 'administrateur');
 
 -- --------------------------------------------------------
 
@@ -90,13 +122,23 @@ CREATE TABLE IF NOT EXISTS `droits` (
 
 DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `id_droits` int(11) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `avatar` varchar(255) DEFAULT 'avatars/defaut.png',
+  `id_droits` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `password`, `email`, `avatar`, `id_droits`) VALUES
+(1, 'amine', '$2y$10$IMIti/SFGVE3p0XobuTKyOZSp4KM79chLHMbNt9pcSkfNtFWJfg92', 'amine@yahoo.fr', 'avatars/defaut.png', 1),
+(4, 'admin', '$2y$10$z4kIn2SLnA9U5Bxn3Hce3O8Dlf.5P4WSxULOWSG/B69pBwCpltzqm', 'admin@gmail.fr', 'avatars/defaut.png', 1337),
+(5, 'modo', '$2y$10$54o4clSos7oXLFahg/8OD.RQkfCJ4Q84Ahm9XAPRLDlHoNoMrK/6q', 'modo@gmail.fr', 'avatars/defaut.png', 42);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
