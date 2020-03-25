b398ae5cc7170c42bd4a06d839b05e03bc48a32e
Lastscream53 <55872979+amine-bouchakour@users.noreply.github.com>
2020-03-25 13:55:36 +0100

    Add files via upload
    fusion fichier serge et amine

--- fonctions.php
+++ fonctions.php
@@ -1,11 +1,126 @@
 <?php
 
+function inscription()
+{
+
+    if(!isset($_SESSION['login']) ){
+
+
+        if(isset($_POST['valider']))
+        {
+        
+            if(!empty($_POST['login']) and !empty($_POST['password']) and !empty($_POST['email']) and !empty($_POST['confirmpassword']) )
+            {
+        
+                $connexion=mysqli_connect('localhost','root','','blog');
+                $requete0="SELECT * FROM utilisateurs WHERE login='".$_POST['login']."'";
+                $query0=mysqli_query($connexion,$requete0);
+                $resultat0=mysqli_fetch_row($query0);
+                var_dump($resultat0);
+        
+                if($resultat0==0)
+                {
+        
+                    if($_POST['password']==$_POST['confirmpassword'])
+                    {
+                        $password=$_POST['password'];
+                        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+                        $requete= "INSERT INTO utilisateurs (login,password,email, id_droits) VALUES ('".$_POST['login']."','".$hashed_password."','".$_POST['email']."',1)";
+                        $query=mysqli_query($connexion,$requete);
+                        echo $requete.'<br/>';
+                        header('location:connexion.php');
+
+                    }
+            
+                    else
+                    {
+                        echo '<div class="erreur">Mot de passe et confirmation de mot de passe différents.</div>'.'</br>';
+                    }
+        
+                }
+        
+                else
+                {
+                    echo '<div class="erreur">Le login est déjà existant, merci de le modifier et de réessayer de nouveau.</div>'.'<br/>';
+                }
+        
+            }
+        
+        else
+            {
+                echo '<div class="erreur">Veuillez remplir tous les champs.</div>'.'</br>';
+            }
+        
+        }
+    }
+    else
+    { header('location:index.php');}
+
+
+
+}
 
 
 
 
 
 
+
+
+function connexion()
+
+{
+    if(!isset($_SESSION['login'])){
+        
+
+        if(isset($_POST['valider']))
+        {
+            if(!empty($_POST['login']) and !empty($_POST['password']))
+
+            {
+
+                $connexion=mysqli_connect('Localhost','root','','blog');
+                $requete= "SELECT id,login,password FROM utilisateurs WHERE login='".$_POST['login']."'";
+                $query=mysqli_query($connexion,$requete);
+                $resultat=mysqli_fetch_row($query);
+                var_dump($resultat);
+
+                $password=$_POST['password'];
+
+
+                if($resultat==0)
+                {
+                        echo '<div class="erreur">Login inexistant</div>'.'<br/>';
+                }
+
+                else
+                {
+
+                    if($_POST['login']==$resultat[1])
+                    {
+                        if(password_verify($password, $resultat[2]))
+                        {
+                            session_start();
+                            $_SESSION['id']=$resultat[0];
+                            $_SESSION['login']=$resultat[1];
+                            header('location:articles.php');
+                        }
+                        else
+                        {
+                            echo '<div class="erreur">Mot de passe incorrecte</div>'.'<br/>';
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+    }
+    else
+    { header('location:index.php');}
+
+}
 
 
 
--- inscription.php
+++ inscription.php
@@ -1,20 +1,38 @@
 <html>
 
-<head>
-<title>Inscritpion</title>
-<link rel="stylesheet" href="style.css">
-</head>
+	<head>
+		<title>Inscription</title>
+		<link rel="stylesheet" type="text/css" href="blog.css">
+	</head>
+
+	<body id="">
+		<?php include('header.php'); ?>
+		<main id="">
+			
+			<div id="">
+				<div id="">INSCRIPTION</div>
+				<form action="" method="post" ><br />
+					
+					<div id="">
+						Login :<br /> <input type="text" name="login"><br />
+						Email :<br /> <input type="text" name="email"><br />
+						Mdp :<br /><input type="password" name="password"><br />
+						Conf mdp : <br /><input type="password" name="confirmpassword"><br />
+					</div>
+					<div id="">
+						<input type="submit" name="valider">
+					</div>
+						
+				</form>
+			<?php
+
+			include('fonctions.php');
+			inscription();
+			?>
+			</div>
 
 
+		</main>
 
-
-
-<?php
-
-?>
-
-
-
-
-
+	</body>
 </html>
